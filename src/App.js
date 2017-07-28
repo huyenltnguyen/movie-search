21047ac142de6f12ed0cad4a3461f27ce0a06d95
huyenltnguyen <huyenltnguyen@outlook.com>
2017-07-28 10:11:27 +0700

    setup the app

---
+++ src/App.js
@@ -0,0 +1,149 @@
+//jshint ignore: start
+import React from 'react';
+import Autosuggest from 'react-autosuggest';
+import MovieDetails from './components/MovieDetails'
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: '',
+			suggestions: [],
+			movieId: '671',
+			movieTitles: []
+		};
+	}
+
+	componentDidMount() {
+		this.getMovieData();
+	}
+
+	getMovieData = () => {
+		const url = `https://api.themoviedb.org/3/movie/${this.state.movieId}?&api_key=`;
+
+		return fetch(url)
+					.then(response => response.json())
+					.then(responseJson => {
+						this.setState({ selectedMovie: responseJson });
+						//console.log('fetched selected movie data:', this.state.selectedMovie);
+					});
+
+	}
+
+	getMovieTitles = () => {
+		const url = `https://api.themoviedb.org/3/search/movie?query=${this.state.value}&api_key=`;
+
+		return fetch(url)
+						.then(response => response.json())
+						.then(responseJson => {
+							this.setState({ movieTitles: responseJson.results.slice(0, 10) });
+							//console.log('fetched movie titles for search:', this.state.movieTitles);
+						});
+	}
+
+	// Change value everytime input changes
+	onChange = (event, { newValue }) => {
+		this.setState({
+			value: newValue
+		});
+
+		if (this.state.value.trim().length >= 2) {
+			setTimeout(function() {
+				this.getMovieTitles();
+			}.bind(this), 300);
+		}
+	}
+
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  getSuggestions = value => {
+	  const inputValue = value.trim().toLowerCase();
+	  const inputLength = inputValue.length;
+	  const movieTitles = this.state.movieTitles;
+
+	  return inputLength === 0 ? [] : movieTitles.filter(movie =>
+	    movie.title.toLowerCase().slice(0, inputLength) === inputValue
+	  ).slice(0, 5);
+	}
+
+	// When suggestion is clicked, Autosuggest needs to populate the input
+	// based on the clicked suggestion. Teach Autosuggest how to calculate the
+	// input value for every given suggestion.
+	getSuggestionValue = suggestion => suggestion.title
+
+	// Use your imagination to render suggestions.
+  renderSuggestion = suggestion => {
+  		const valueLen = this.state.value.length;
+  		const suggestionLen = suggestion.title.length;
+	  	return (
+	  		<span><span className="matched">{ this.state.value }</span>{ suggestion.title.slice(valueLen, suggestionLen) }</span>
+  		);
+	}
+
+	// Autosuggest will call this function every time you need to update suggestions.
+  onSuggestionsFetchRequested = ({ value }) => {
+    setTimeout(() => {
+      const suggestions = this.getSuggestions(value);
+      this.setState({
+          suggestions
+      });
+    }, 1000);
+  }
+
+	// Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+      movieTitles: []
+    });
+    //console.log('movie titles on clear:', this.state.movieTitles);
+  }
+
+  // Autosuggest will call this function every time suggestion is selected via mouse or keyboard.
+  onSuggestionSelected = (event, { suggestion }) => {
+    //console.log('Movie selected:', suggestion);
+    this.setState({
+    	movieId: suggestion.id,
+  	});
+
+  	setTimeout(function() {
+				this.getMovieData();
+			}.bind(this), 500);
+  }
+
+	render() {
+		const { value, suggestions } = this.state;
+
+		const inputProps = {
+			placeholder: 'Search Movie Title...',
+			value,
+			onChange: this.onChange
+		};
+
+		if (!this.state.selectedMovie) {return <p className="loading-text text-center">Loading...</p>}
+
+		return (
+			<div className="App container">
+				<div className="col-md-10 col-md-offset-1">
+					<div className="row">
+						<div className="search-bar col-md-5">
+							<Autosuggest
+				        suggestions={suggestions}
+				        onSuggestionsFetchRequested={ this.onSuggestionsFetchRequested }
+				        onSuggestionSelected={ this.onSuggestionSelected }
+				        onSuggestionsClearRequested={ this.onSuggestionsClearRequested }
+				        getSuggestionValue={ this.getSuggestionValue }
+				        renderSuggestion={ this.renderSuggestion }
+				        inputProps={ inputProps }
+							/>
+						</div>
+					</div>
+
+					<MovieDetails movieData={this.state.selectedMovie} />
+					</div>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/MovieDetails.js
@@ -0,0 +1,88 @@
+// jshint ignore: start
+import React from 'react';
+
+const MovieDetails = (props) => {
+	const movieData = props.movieData;	
+
+	//--------- set backdrop as body background -------//
+	if (movieData.backdrop_path) {
+		const backdropURL = `https://image.tmdb.org/t/p/original${movieData.backdrop_path}`;
+
+		document.body.classList.add('backdrop');
+		document.body.style = `background-image: url(${backdropURL});
+														background-size: cover;
+														background-repeat: no-repeat
+														`;
+	} else {
+		document.body.style = 'background-color: #242424';
+	}
+
+	//--------- format Vote Average --------//
+	let vote_average = movieData.vote_average;
+
+	if (vote_average === 0) {
+		vote_average = '-';
+	} else {
+		vote_average += '/10';
+	}
+
+
+	//--------- format Vote Average --------//
+	let revenue = movieData.revenue;	
+
+	if (revenue === 0) {
+		revenue = '-';
+	} else {
+		revenue = `$${revenue.toLocaleString()}`;
+	}
+
+	return (
+		<div className="movie-details row">						
+			<div className="col-md-5 poster-container">
+				<img className="poster" src={'https://image.tmdb.org/t/p/w500' + movieData.poster_path} alt=""/>
+			</div>
+
+			<div className="col-md-7 details-container">
+				<h1>{movieData.title}</h1>
+
+				<h2 className="genres">
+					{ movieData.genres.map(genre => {
+							return <span key={genre.id} className="genre">{genre.name}</span>
+						})
+				 	}
+				</h2>
+
+				<h3>Overview:</h3>
+				<p className="overview-content">{ movieData.overview }</p>
+
+				<div className="row">
+					<div className="col-md-6">
+						<h3>Release Date:</h3>
+						<p>{ movieData.release_date }</p>
+					</div>
+
+					<div className="col-md-6">
+						<h3>Running Time:</h3>
+						<p>{ movieData.runtime } mins</p>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-md-6">
+						<h3>Box Office:</h3>
+						<p>{ revenue }</p>
+					</div>
+
+					<div className="col-md-6">
+						<h3>Vote Average:</h3>
+						<p>{ vote_average }</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	);
+	
+};
+
+export default MovieDetails;
